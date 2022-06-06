de6b1d75361b5f0ddad38d62039ef4472e16cbd7
snowflakelearning836 <106194221+snowflakelearning836@users.noreply.github.com>
2022-06-06 17:27:52 -0400

    Update streamlit_app.py
    Let's put a pick list here so they can pick the fruit they want to include

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,10 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocade Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# getting table data from csv
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+# display the table on the page
 streamlit.dataframe(my_fruit_list)
 
